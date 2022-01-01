a267fb2e210bc08f81be641378bd76ff0cbef126
Aseem Mishra <aseemmishra@Aseems-MacBook-Air.local>
2022-01-01 14:25:15 +0530

    header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 function App() {
   return (
-    <div>
+    <main>
       <Header />
-      
-      
+      <Container>
       <h1>Welcome to the superstore</h1>
 
+
+      </Container>
+      
+      
       <Footer/>
     
     
     
-    </div>
+    </main>
   );
 }
 
