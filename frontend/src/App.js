2f6e3be7033e7f725d476bb438b40ef4d704cc82
Aseem Mishra <aseemmishra@Aseems-MacBook-Air.local>
2022-01-01 14:02:02 +0530

    react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+function App() {
+  return (
+    <div>
+      <Header />
+      
+      
+      <h1>Welcome to the superstore</h1>
+
+      <Footer/>
+    
+    
+    
+    </div>
+  );
+}
+
+export default App;
