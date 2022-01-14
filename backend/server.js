a8eb743dea86efeec189610a7ea80689f701a492
aseemmishra13 <mishraaseem05@gmail.com>
2022-01-14 21:58:45 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,10 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 app.use(express.json())
+app.use('/api/products',productRoutes)
+app.use('/api/users',userRoutes)
+app.use('/api/orders',orderRoutes)
+app.use('/api/upload',uploadRoutes)
 const __dirname=path.resolve()
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
@@ -32,10 +36,6 @@ if (process.env.NODE_ENV==='production'){
 app.get('/',(req,res)=>{
     res.send('api is running')
 })}
-app.use('/api/products',productRoutes)
-app.use('/api/users',userRoutes)
-app.use('/api/orders',orderRoutes)
-app.use('/api/upload',uploadRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
